7df74eec83b051081db8512b1540ce18420b99d0
HF-PC\HF <809969840@qq.com>
2022-07-17 21:55:46 +0800

    测试1

--- testDemo/src/main/java/com/hufei/Test1.java
+++ testDemo/src/main/java/com/hufei/Test1.java
@@ -10,7 +10,7 @@ package com.hufei;
 public class Test1 {
 
     public static void main(String[] args) {
-        System.out.println("测试11111111111");
+        System.out.println("测试11111111111222");
     }
 
 }
